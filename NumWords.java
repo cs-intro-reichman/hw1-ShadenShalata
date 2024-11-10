be53d9eef15f0c8b26d2fc2d0a04ac85e3683fa5
shadin shalaata <shadinshalaata@shadins-MacBook-Pro.local>
2024-11-10 14:47:49 +0200

    Done NumWords

--- NumWords.java
+++ NumWords.java
@@ -2,5 +2,11 @@
 public class NumWords {
 	public static void main(String args[]) {
 	    // Replace this comment with your code
+		int Num = Integer.parseInt(args[0]);
+		int ones=Num%10;
+		int tens=(Num/10)%10;
+		int hund=(Num/100);
+		System.out.println(hund+" hundreds, "+tens+" tens, and "+ones+" ones.");
+		
 	}
 }
