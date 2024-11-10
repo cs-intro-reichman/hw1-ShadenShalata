48af3aa2778b9b9989ce4944c1f2aa90ae9f5095
shadin shalaata <shadinshalaata@shadins-MacBook-Pro.local>
2024-11-10 15:29:32 +0200

    Done Ascend

--- Ascend.java
+++ Ascend.java
@@ -3,5 +3,19 @@
 public class Ascend {
 	public static void main(String[] args) {
 		// Replace this comment with your code
+		int lim=Integer.parseInt(args[0]);
+		int a=(int)(Math.random()*lim);
+		int b=(int)(Math.random()*lim);
+		int c=(int)(Math.random()*lim);
+		int sum=a+b+c;
+		int min1=Math.min(a,b);
+		int minimum=Math.min(c,min1);
+		int max1=Math.max(a,b);
+		int maximum=Math.max(c,max1);
+		int mid=sum-minimum-maximum;
+		System.out.println(a+" "+b+" "+c);
+		System.out.println(minimum+" "+mid+" "+maximum);
+
+
 	}
 }
